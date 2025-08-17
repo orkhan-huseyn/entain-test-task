8d667c0fa1c67b45aff9c2e6610957ae65db2c77
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2025-08-17 18:09:08 +0400

    feat: add request logger middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -2,11 +2,11 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /user/{userId}/transaction", app.createTransaction)
 	mux.HandleFunc("GET /user/{userId}/balance", app.getUserBalance)
 
-	return mux
+	return app.logRequest(mux)
 }
